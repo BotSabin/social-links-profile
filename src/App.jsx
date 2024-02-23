0947a26ece4bd18d82a23527fbd2774ecebdc16b
Botify <152845982+BotifyS@users.noreply.github.com>
2024-02-23 08:08:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom"
 function App() {
 
   return (
-    <div className="px-5 flex flex-col justify-center items-center min-h-screen font-inter">
+    <main className="px-5 flex flex-col justify-center items-center min-h-screen font-inter">
     <section className="bg-hsl-12 w-full max-w-[375px] p-8 rounded-lg">
         <div className="flex flex-col items-center gap-y-6">
           <img src="avatar-jessica.jpeg" alt=""
            className="rounded-full" width={85} />
            <div className="flex flex-col items-center gap-y-2">
           <h1 className="text-hsl-100 text-2xl">Jessica Randall</h1>
-          <h3 className="text-hsl-green font-bold text-[13px]">London, United Kingdom</h3>
+          <h2 className="text-hsl-green font-bold text-[13px]">London, United Kingdom</h2>
            </div>
           <p className="text-sm text-white/70 pb-6">&quot;Front-end developer and avid reader.&quot;</p>
         </div>
@@ -23,7 +23,7 @@ function App() {
           <Link to="#" className="bg-hsl-20 w-full py-3 rounded-lg hover:bg-hsl-green hover:text-hsl-12 transition-colors duration-300">Instagram</Link>
         </div>
     </section>
-    </div>
+    </main>
   )
 }
 
